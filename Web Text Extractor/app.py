78093ef61d6023ef1ea49aad889a0f9d38e6d0f5
Prats-23 <93593351+Prats-23@users.noreply.github.com>
2023-04-20 01:32:21 +0530

    Changed style

--- Web Text Extractor/app.py
+++ Web Text Extractor/app.py
@@ -1,24 +1,23 @@
-from flask import Flask, request
+from flask import *
 import requests
 from bs4 import BeautifulSoup
+from newspaper import Article
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def get_text():
+    text = ''
     if request.method == 'POST':
         url = request.form['url']
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        text = soup.get_text()
-        return text
-    return '''
-        <form method="post">
-            <label for="url">Enter the URL of a website:</label>
-            <input type="text" id="url" name="url">
-            <input type="submit" value="Submit">
-        </form>
-    '''
+        article = Article(url,language="en")
+        article.download()
+        article.parse()
+        article.nlp()
+        text=article.text
+
+        # return render_template('text.html', text=text)
+    return render_template('./index.html',text=text)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Web Text Extractor/get_website_text.py
+++ Web Text Extractor/get_website_text.py
@@ -1,11 +1,26 @@
+from langcodes import Language
+from regex import P
 import requests
 from bs4 import BeautifulSoup
+from newspaper import Article
 
-url = "https://www.dictionary.com/browse/review"
+url = "https://us.cnn.com/2023/04/19/us/dadeville-alabama-birthday-party-shooting-wednesday/index.html"
 
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-text = soup.get_text()
+# response = requests.get(url)
+# soup = BeautifulSoup(response.content, 'html.parser')
+# text = soup.find('html')
+# print(text)
 
 
-print(text)
+# # print(text)
+# print(soup.prettify())
+
+article = Article(url,language="en")
+article.download()
+article.parse()
+article.nlp()
+print(article.title)
+print(article.text)
+print(article.summary)
+print(article.keywords)
+
