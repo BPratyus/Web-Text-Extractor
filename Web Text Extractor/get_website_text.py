d4144afc3c74f764efae5074f0d0f6a4c3df3121
Prats-23 <93593351+Prats-23@users.noreply.github.com>
2023-04-20 01:31:02 +0530

    Added CSS

--- Web Text Extractor/get_website_text.py
+++ Web Text Extractor/get_website_text.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = input("https://www.dictionary.com/browse/review")
+url = "https://www.dictionary.com/browse/review"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 text = soup.get_text()
+
 
 print(text)
