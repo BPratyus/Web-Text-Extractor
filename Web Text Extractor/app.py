f66d8861d4da430674d5f161645ae41897c82f3b
Prats-23 <93593351+Prats-23@users.noreply.github.com>
2023-04-20 21:34:03 +0530

    Added article summary, title and keywords

--- Web Text Extractor/app.py
+++ Web Text Extractor/app.py
@@ -1,6 +1,6 @@
 from flask import *
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
 from newspaper import Article
 
 app = Flask(__name__)
@@ -8,16 +8,20 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def get_text():
     text = ''
+    title=''
+    keywords=''
     if request.method == 'POST':
         url = request.form['url']
         article = Article(url,language="en")
         article.download()
         article.parse()
         article.nlp()
-        text=article.text
+        title = article.title
+        text=article.summary
+        keywords=article.keywords
 
         # return render_template('text.html', text=text)
-    return render_template('./index.html',text=text)
+    return render_template('./index.html',title=title,text=text,keywords=keywords)
 
 if __name__ == '__main__':
     app.run(debug=True)
