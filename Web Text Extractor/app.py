3b06e9a78a4d792884ba3ae48a85a67d35e343f2
Prats-23 <93593351+Prats-23@users.noreply.github.com>
2023-04-27 19:12:45 +0530

    Adeed SpaCy integration

--- Web Text Extractor/app.py
+++ Web Text Extractor/app.py
@@ -1,15 +1,26 @@
 from flask import *
+# from keras.models import load_model
 # import requests
 # from bs4 import BeautifulSoup
 from newspaper import Article
 
+import spacy
+from spacy import displacy
+nlp=spacy.load('en_core_web_sm')
+from flaskext.markdown import Markdown
+
+# model = load_model(r'D:\VIT\Sem 6\NLP Lab\Web Text Extractor\Web Text Extractor\nerlstm.h5')
+# model.summary()
 app = Flask(__name__)
+Markdown(app)
+
+HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem;">{}</div>"""
 
 @app.route('/', methods=['GET', 'POST'])
 def get_text():
     text = ''
     title=''
-    keywords=''
+    result=''
     if request.method == 'POST':
         url = request.form['url']
         article = Article(url,language="en")
@@ -17,11 +28,15 @@ def get_text():
         article.parse()
         article.nlp()
         title = article.title
-        text=article.summary
-        keywords=article.keywords
+        text=article.text
+        docx = nlp(text)
+        html = displacy.render(docx,style='ent')
+        html=html.replace("\n\n","\n")
+        result = HTML_WRAPPER.format(html)
+        
 
         # return render_template('text.html', text=text)
-    return render_template('./index.html',title=title,text=text,keywords=keywords)
+    return render_template('./index.html',title=title,text=text,result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=80)
